9aa6b13bf13fb8fef47771f324bbb3f7bf507da8
ToshY <31921460+ToshY@users.noreply.github.com>
2025-02-21 20:35:58 +0100

    fixes deprecation with extension dependency

--- DependencyInjection/LexikMaintenanceExtension.php
+++ DependencyInjection/LexikMaintenanceExtension.php
@@ -5,7 +5,7 @@ namespace Lexik\Bundle\MaintenanceBundle\DependencyInjection;
 use Symfony\Component\DependencyInjection\Exception\InvalidArgumentException;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 /**
